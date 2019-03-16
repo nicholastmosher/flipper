22fd106882a98f50aa3b18c8ecdfd3be77b6bc17
Nick Mosher <nicholastmosher@gmail.com>
2019-03-16 15:31:33 -0700

    Flipper modules are extension traits of Client

--- library/rust/examples/gpio.rs
+++ library/rust/examples/gpio.rs
@@ -1,47 +1,39 @@
 use flipper::{Client, LfType, Args, Flipper};
 use libusb::Context;
 
-struct Gpio<'a, T: Client> {
-    device: &'a mut T,
-}
-
-impl<'a, T: Client> Gpio<'a, T> {
-    pub fn new(device: &'a mut T) -> Gpio<'a, T> { Gpio { device } }
-
-    pub fn configure(&mut self) {
-        self.device.invoke("gpio", 3, LfType::lf_void, &Args::new());
+trait Gpio: Client {
+    fn gpio_configure(&mut self) {
+        self.invoke("gpio", 3, LfType::lf_void, &Args::new());
     }
 
-    pub fn enable(&mut self, enabled: u32, disabled: u32) {
+    fn gpio_enable(&mut self, enabled: u32, disabled: u32) {
         let mut args = Args::new();
         args.append(enabled)
             .append(disabled);
-        self.device.invoke("gpio", 2, LfType::lf_void, &args);
+        self.invoke("gpio", 2, LfType::lf_void, &args);
     }
 
-    pub fn write(&mut self, high: u32, low: u32) {
+    fn gpio_write(&mut self, high: u32, low: u32) {
         let mut args = Args::new();
         args.append(high)
             .append(low);
-        self.device.invoke("gpio", 1, LfType::lf_void, &args);
+        self.invoke("gpio", 1, LfType::lf_void, &args);
     }
 
-    pub fn read(&mut self, pins: u32) -> u32 {
+    fn gpio_read(&mut self, pins: u32) -> u32 {
         let mut args = Args::new();
         args.append(pins);
-        self.device.invoke("gpio", 0, LfType::lf_uint32, &args)
+        self.invoke("gpio", 0, LfType::lf_uint32, &args)
             .expect("should get read result") as u32
     }
 }
 
-fn main() {
+impl<T> Gpio for T where T: Client { }
 
+fn main() {
     let mut context = Context::new().expect("should get usb context");
     let mut flippers = Flipper::attach_usb(&mut context);
     let flipper = flippers.first_mut().expect("should find one Flipper");
 
-    let mut gpio = Gpio::new(flipper);
-
-    let result = gpio.read(0);
-    println!("Result is {}", result);
+    flipper.gpio_write(1 << 22, 0);
 }
--- library/rust/examples/led.rs
+++ library/rust/examples/led.rs
@@ -1,29 +1,23 @@
 use flipper::{Client, LfType, Args, Flipper};
 use libusb::Context;
 
-struct Led<'a, T: Client> {
-    device: &'a mut T,
-}
-
-impl<'a, T: Client> Led<'a, T> {
-    pub fn new(device: &'a mut T) -> Led<'a, T> {
-        Led { device }
-    }
-
-    pub fn rgb(&mut self, red: u8, green: u8, blue: u8) {
+trait Led: Client {
+    fn led_rgb(&mut self, red: u8, green: u8, blue: u8) {
         let mut args = Args::new();
         args.append(red)
             .append(green)
             .append(blue);
-        self.device.invoke("led", 0, LfType::lf_void, &args);
+        self.invoke("led", 0, LfType::lf_void, &args);
     }
 }
 
+impl<T> Led for T where T: Client { }
+
 fn main() {
     let mut context = Context::new().expect("should get usb context");
+
     let mut flippers = Flipper::attach_usb(&mut context);
     let flipper = flippers.first_mut().expect("should find one flipper");
 
-    let mut led = Led::new(flipper);
-    led.rgb(10, 05, 10);
+    flipper.led_rgb(10, 05, 10);
 }
